01feb39d030d0fa3ed3edf7f0df1bfe3f81cdab7
Chris Weiss <bitblit@gmail.com>
2018-05-22 14:41:34 -0700

    Cleanup test

--- test/test-ports-of-call.ts
+++ test/test-ports-of-call.ts
@@ -4,6 +4,7 @@ import {PortsOfCall} from "../src/ports-of-call";
 describe('#portsOfCall', function() {
     it('should return an instance from the Instance function', function() {
         let result = PortsOfCall.Instance;
+        result.abort();
         expect(result).to.not.be.null;
     });
 });
